49ec3e59a51742986f44a567006b0bb66715d4b2
monsterpumpkin000-lang <monsterpumpkin000@gmail.com>
2026-01-02 18:37:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import os
 
 app = FastAPI()
 
+from openai import OpenAI
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
 # =========================
 # TTS
 # =========================
